887888d8ce0d8fe67efba950f55649c023896ec3
svshp <svs_hp@i.ua>
2018-10-29 13:16:23 +0200

    version: 1.1.0

--- src/js/main.js
+++ src/js/main.js
@@ -35,8 +35,11 @@ function setUpButtons() {
     let btnUpdate = document.querySelector('.block-header__btn-update');
     btnUpdate.addEventListener('click', updateEvent);
 
-    let btnFind = document.querySelector('.block-header__find_find');
-    btnFind.addEventListener('click', findEvents);
+    let fieldFind = document.querySelector('.block-header__find_field');
+    fieldFind.addEventListener('keyup', fieldFindEvents);
+
+    let btnClear = document.querySelector('.block-header__find_btn-clear');
+    btnClear.addEventListener('click', fieldFindBtnClear);
 
     let btnMonthMinus = document.querySelector('.section-dates__select-period_month-minus');
     btnMonthMinus.addEventListener('click', monthMinus);
@@ -57,7 +60,82 @@ function updateEvent() {
 }
 
 function findEvents() {
-    console.log('findEvents()');
+    let fieldFindEvents = document.querySelector('.block-header__find_field');
+
+    let findEvents = searchTitleEvents();
+
+    findEvents.style.top = (fieldFindEvents.offsetTop + fieldFindEvents.offsetHeight + 13) + 'px';
+    findEvents.style.left = fieldFindEvents.offsetLeft + 'px';
+    findEvents.style.display = 'block';
+}
+
+function searchTitleEvents() {
+    let findEvents = getFindEvents();
+    let searchValue = document.querySelector('.block-header__find_field').value.toLowerCase();
+
+    let resultSearch = storage.tableEvents.filter((item) => {
+        return item.title.toLowerCase().search(searchValue) !== -1;
+    });
+
+    for (let i = 0; i < resultSearch.length; i++) {
+        let findEventsRow = document.createElement('div');
+        findEventsRow.classList.add('find-events__row');
+
+        let rowTitle = document.createElement('div');
+        rowTitle.textContent = resultSearch[i].title;
+        rowTitle.classList.add('find-events__row_title');
+
+        let rowInfo = document.createElement('div');
+        rowInfo.textContent = moment(resultSearch[i].date).format('Do MMMM');
+        rowInfo.classList.add('find-events__row_Info');
+
+        findEventsRow.appendChild(rowTitle);
+        findEventsRow.appendChild(rowInfo);
+
+        findEvents.appendChild(findEventsRow);
+    }
+
+    return findEvents;
+}
+
+function getFindEvents() {
+    let findEvents = document.querySelector('.find-events');
+
+    while (findEvents.hasChildNodes()) {   
+        findEvents.removeChild(findEvents.firstChild);
+    }
+
+    return findEvents;
+}
+
+function fieldFindEvents(e) {
+    let searchingText = document.querySelector('.block-header__find_field').value;
+
+    let btnClear = document.querySelector('.block-header__find_btn-clear');
+    if (searchingText.length < 3) {
+        btnClear.style.color = '#fff';
+        hideFindEvents();
+    }
+    else {
+        btnClear.style.color = '#000';
+
+        findEvents();
+    }
+}
+
+function fieldFindBtnClear() {
+    document.querySelector('.block-header__find_field').value = '';
+
+    hideFindEvents();
+
+    let btnClear = document.querySelector('.block-header__find_btn-clear');
+    btnClear.style.color = '#fff';
+    btnClear.blur();
+}
+
+function hideFindEvents() {
+    let findEvents = document.querySelector('.find-events');
+    findEvents.style.display = 'none';
 }
 
 function monthMinus() {
@@ -110,7 +188,7 @@ function calculateDates() {
         storage.endDate.add(-7, 'days');
         storage.longPeriod = false;
     }
-    else{
+    else {
         storage.longPeriod = true;
     }
 }
@@ -147,7 +225,7 @@ function correctLayoutTable(prevMaxRows) {
 }
 
 function generateTableRow() {
-    let tableRow = document.createElement( 'div' );
+    let tableRow = document.createElement('div');
 
     for (let y = 0; y < 7; y++) {
         let sectionDatesTableCol = generateTableRowCol();
@@ -160,10 +238,10 @@ function generateTableRow() {
 }
 
 function generateTableRowCol() {
-    let tableRowCol = document.createElement( 'div' );
-    let span = document.createElement( 'span' );
-    let div1 = document.createElement( 'div' );
-    let div2 = document.createElement( 'div' );
+    let tableRowCol = document.createElement('div');
+    let span = document.createElement('span');
+    let div1 = document.createElement('div');
+    let div2 = document.createElement('div');
 
     div1.classList.add('section-dates__table_col-event');
     div2.classList.add('section-dates__table_col-participant');
