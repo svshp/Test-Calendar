cee93afda2de67f4bd7e18c0f3df6b6dc27c1045
svshp <svs_hp@i.ua>
2018-10-31 22:24:52 +0200

    version: 1.4.0

--- src/js/main.js
+++ src/js/main.js
@@ -18,11 +18,11 @@ let storage = {
 window.onload = function () {
     storage.curentMonth = moment().date(1).hours(0).minutes(0).seconds(0);
 
-    setUpButtons();
     calculateDates();
     buildLayoutTable();
     outputPeriod();
     readData();
+    setUpButtons();
 }
 
 //**********************************************************************
@@ -49,13 +49,59 @@ function setUpButtons() {
 
     let btnToday = document.querySelector('.section-dates__select-period_today');
     btnToday.addEventListener('click', setToday);
+
+    let btnClose = document.querySelector('.add-events__body_btn-close');
+    btnClose.addEventListener('click', addEventsBtnClose);
+
+    let btnCreate = document.querySelector('.add-events__body_btn-create');
+    btnCreate.addEventListener('click', addEventsBtnCreate);
+
+    let btnOk = document.querySelector('.add-events__body_btn-ok');
+    btnOk.addEventListener('click', addEventsBtnOk);
+
+    let btnDelete = document.querySelector('.add-events__body_btn-delete');
+    btnDelete.addEventListener('click', addEventsBtnDelete);
+
+    let tableCol = document.querySelectorAll('.section-dates__table_col');
+    for (let i =0; i < tableCol.length; i++) {
+        tableCol[i].addEventListener('dblclick', editEvent);
+    }
 }
 
 //**********************************************************************
 // Events
 
 function addEvent() {
-    console.log('addEvent()');
+    let btnAddEvent = document.querySelector('.block-header__btn-add');
+    let addEvent = document.querySelector('.add-events');
+
+    document.querySelector('.add-events__body_btn-ok').style.display = 'none';
+    document.querySelector('.add-events__body_btn-delete').style.display = 'none';
+
+    addEvent.style.top = (btnAddEvent.offsetTop + btnAddEvent.offsetHeight + 10) + 'px';
+    addEvent.style.left = btnAddEvent.offsetLeft + 'px';
+    addEvent.style.height = '500px';
+    addEvent.style.width = '350px';
+    addEvent.style.display = 'block';
+}
+
+function addEventsBtnClose() {
+    closeAddEvents();
+}
+
+function addEventsBtnCreate() {
+    if (verifeEvent()) {
+        createEvent();
+        closeAddEvents();
+    }
+}
+
+function addEventsBtnOk() {
+    console.log('addEventsBtnOk()');
+}
+
+function addEventsBtnDelete() {
+    console.log('addEventsBtnDelete()');
 }
 
 function updateEvent() {
@@ -70,6 +116,7 @@ function monthMinus() {
     calculateDates();
     correctLayoutTable(prevMaxRows);
     outputPeriod();
+    sortTableEvents();
     fillTable();
 }
 
@@ -81,6 +128,7 @@ function monthPlus() {
     calculateDates();
     correctLayoutTable(prevMaxRows);
     outputPeriod();
+    sortTableEvents();
     fillTable();
 }
 
@@ -92,6 +140,7 @@ function setToday() {
     calculateDates();
     correctLayoutTable(prevMaxRows);
     outputPeriod();
+    sortTableEvents();
     fillTable();
 }
 
@@ -125,9 +174,10 @@ function fieldFindBtnClear() {
     btnClear.blur();
 }
 
-function hideFindEvents() {
-    let findEvents = document.querySelector('.find-events');
-    findEvents.style.display = 'none';
+function editEvent(e) {
+    console.log(e.path[0]);
+    console.log(e.path[1]);
+    console.log('editEvent()');
 }
 
 //**********************************************************************
@@ -218,35 +268,36 @@ function fillTable() {
 
         for (let c = 0; c < childrenRow.children.length; c++) {
             let curTableCol = childrenRow.children[c];
-            let curTableColSpan = curTableCol.querySelector('span');
 
             curTableCol.classList.remove('today');
             curTableCol.classList.remove('event');
 
-            curTableColSpan.textContent = countDate < 7 ? storage.outputDate.format('dddd') + ', ' : '';
-            curTableColSpan.textContent += storage.outputDate.date();
-
             if (equalToday(storage.outputDate.format(storage.curentFormat))) {
                 curTableCol.classList.add('today');
             }
+        
+            let curTableColSpan = curTableCol.querySelector('span');
 
+            curTableColSpan.textContent = countDate < 7 ? storage.outputDate.format('dddd') + ', ' : '';
+            curTableColSpan.textContent += storage.outputDate.date();
+        
             let curTableColEvent = curTableCol.querySelector('.section-dates__table_col-event');
             curTableColEvent.textContent = '';
             let curTableColParticipants = curTableCol.querySelector('.section-dates__table_col-participant');
             curTableColParticipants.textContent = '';
-
+        
             if ((firstEventIndex !== -1) && (firstEventIndex < storage.tableEvents.length)) {
                 if (equalDates(firstEventIndex)) {
                     curTableColEvent.textContent = storage.tableEvents[firstEventIndex].title;
-
+        
                     curTableCol.classList.add('event');
-
+        
                     addParticipants(curTableColParticipants, firstEventIndex);
-
+        
                     firstEventIndex++;
                 }
             }
-            
+                    
             countDate++;
             storage.outputDate.add(1, 'days');
         }
@@ -259,9 +310,11 @@ function addParticipants(curTableColParticipants, firstEventIndex) {
     let listParticipants = '';
 
     for (let i = 0; i < quantParticipants; i++) {
-        listParticipants += storage.tableEvents[firstEventIndex].participants[i].name
-                         + ' '
-                         + storage.tableEvents[firstEventIndex].participants[i].surname;
+        listParticipants += storage.tableEvents[firstEventIndex].participants[i].name;
+
+        if (storage.tableEvents[firstEventIndex].participants[i].surname !== undefined) {
+            listParticipants += (' ' + storage.tableEvents[firstEventIndex].participants[i].surname);
+        }
 
         if (i === (quantParticipants - 1)) {
             listParticipants += '.';
@@ -274,12 +327,17 @@ function addParticipants(curTableColParticipants, firstEventIndex) {
     curTableColParticipants.textContent = listParticipants;
 }
 
+function hideFindEvents() {
+    let findEvents = document.querySelector('.find-events');
+    findEvents.style.display = 'none';
+}
+
 function findEvents() {
     let fieldFindEvents = document.querySelector('.block-header__find_find');
 
     let findEvents = searchEvents();
 
-    findEvents.style.top = (fieldFindEvents.offsetTop + fieldFindEvents.offsetHeight + 7) + 'px';
+    findEvents.style.top = (fieldFindEvents.offsetTop + fieldFindEvents.offsetHeight + 9) + 'px';
     findEvents.style.left = fieldFindEvents.offsetLeft + 'px';
     findEvents.style.display = 'block';
 }
@@ -349,14 +407,76 @@ function calculateDates() {
     }
 }
 
-function equalDates(firstEventIndex) {
-    return moment(storage.tableEvents[firstEventIndex].date).hours(0).minutes(0).seconds(0).format(storage.curentFormat)
+function closeAddEvents() {
+    let addEvent = document.querySelector('.add-events');
+
+    document.querySelector('.add-events__body_title-field').value = '';
+    document.querySelector('.add-events__body_date-field').value = '';
+    document.querySelector('.add-events__body_participant-field').value = '';
+    document.querySelector('.add-events__body_description-field').value = '';
+
+    addEvent.style.display = 'none';
+}
+
+function createEvent() {
+    let newEvent = {
+        'id': 0,
+        'title': document.querySelector('.add-events__body_title-field').value,
+        'date': document.querySelector('.add-events__body_date-field').value,
+        'participants': [],
+        'description': document.querySelector('.add-events__body_description-field').value,
+    }
+
+    let arrPartisipants = document.querySelector('.add-events__body_participant-field').value.split(',');
+
+    for (let i = 0; i < arrPartisipants.length; i++) {
+        let arrName = arrPartisipants[i].trim().split(' ');
+    
+        try {
+            newEvent.participants.push({
+                'id': 0,
+                'name': arrName[0],
+                'surname': arrName[1]
+            });
+        
+        }
+        catch {}
+    }
+
+    let indexEvent = findDateEvent(newEvent);
+
+    if (indexEvent === -1) {
+        indexEvent = storage.tableEvents.length;
+    }
+
+    storage.tableEvents[indexEvent] = newEvent;
+
+    localStorage.setItem('arrayEvents', JSON.stringify(storage.tableEvents));
+
+    updateTableCol(indexEvent);
+}
+
+function equalDates(indexEvent) {
+    return moment(storage.tableEvents[indexEvent].date).hours(0).minutes(0).seconds(0).format(storage.curentFormat)
            === moment(storage.outputDate).hours(0).minutes(0).seconds(0).format(storage.curentFormat);
 }
 
 function equalToday(curDate) {
     return moment().hours(0).minutes(0).seconds(0).format(storage.curentFormat)
            === curDate;
+}
+
+function findDateEvent(event) {
+    let result = -1;
+
+    for (let i = 0; i < storage.tableEvents.length; i++) {
+        if (moment(storage.tableEvents[i].date).hours(0).minutes(0).seconds(0).format(storage.curentFormat)
+            === moment(event.date).hours(0).minutes(0).seconds(0).format(storage.curentFormat)) {
+            result = i;
+            break;
+        }
+    }
+    return result;
 }
 
 function findFirstEventIndex(startDate) {
@@ -382,8 +502,8 @@ function readData() {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     storage.tableEvents = JSON.parse(xhr.responseText).events;
-                
-                    storage.tableEvents.sort((first, second) => (moment(first.date) - moment(second.date)));
+
+                    sortTableEvents();
 
                     fillTable();
                 }
@@ -398,4 +518,64 @@ function readData() {
 
         fillTable();
     }
+}
+
+function sortTableEvents() {
+    storage.tableEvents.sort((first, second) => (moment(first.date) - moment(second.date)));
+}
+
+function verifeEvent() {
+    if (!document.querySelector('.add-events__body_title-field').value) {
+        alert('Empty title!');
+
+        return false;
+    }
+
+    let dateTime = document.querySelector('.add-events__body_date-field');
+
+    if (!dateTime.value) {
+        if (dateTime.validationMessage) {
+            alert(dateTime.validationMessage);
+        }
+        else {
+            alert('Empty date!');
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+function updateTableCol(indexEvent) {
+    storage.outputDate = moment(storage.startDate);
+
+    let curTable = document.querySelector('.section-dates__table');
+
+    for (let r = 0; r < curTable.children.length; r++) {
+        let childrenRow = curTable.children[r];
+
+        for (let c = 0; c < childrenRow.children.length; c++) {
+            let curTableCol = childrenRow.children[c];
+        
+            if (equalDates(indexEvent)) {
+                addNewEvent(curTableCol, indexEvent);
+            }
+        
+            storage.outputDate.add(1, 'days');
+        }
+    }
+}
+
+function addNewEvent(curTableCol, indexEvent) {
+    curTableCol.classList.add('event');
+
+    let curTableColEvent = curTableCol.querySelector('.section-dates__table_col-event');
+    curTableColEvent.textContent = '';
+    let curTableColParticipants = curTableCol.querySelector('.section-dates__table_col-participant');
+    curTableColParticipants.textContent = '';
+
+    curTableColEvent.textContent = storage.tableEvents[indexEvent].title;
+
+    addParticipants(curTableColParticipants, indexEvent);
 }
